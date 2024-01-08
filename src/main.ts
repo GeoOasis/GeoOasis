04f0db26f1b602e72ef654c5114a9e7dbd20f95c
Knight-wh <yffcbgh@163.com>
2024-01-08 17:24:30 +0800

    feat: install pinia

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import './style/main.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App)
 
+declare global {
+    interface Window {
+        CESIUM_BASE_URL: string
+    }
+}
+const pinia = createPinia()
+app.use(pinia)
 app.use(ElementPlus)
 app.mount('#app')
